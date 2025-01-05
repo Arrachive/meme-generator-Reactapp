62e3224cbc014029db8d9c18dc6b553a920ae06b
Arrachive <agbojoayomide6@gmail.com>
2025-01-05 12:11:36 +0100

    Initial commit of my React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+import Main from "../components/Form"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
